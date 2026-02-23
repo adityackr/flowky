03669e8e437ced3d16eed600df4db9be54dff220
adityackr <aditya09me@gmail.com>
2026-02-23 10:40:00 +0600

    feat: Implement standalone layout and workspace creation page, adding authentication to workspace routes.

--- features/workspaces/components/create-workspace-form.tsx
+++ features/workspaces/components/create-workspace-form.tsx
@@ -13,6 +13,7 @@ import {
 	FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ImageIcon } from 'lucide-react';
 import Image from 'next/image';
@@ -158,6 +159,7 @@ export const CreateWorkspaceForm: FC<CreateWorkspaceFormProps> = ({
 								variant="secondary"
 								onClick={onCancel}
 								disabled={isPending}
+								className={cn(!onCancel && 'invisible')}
 							>
 								Cancel
 							</Button>
